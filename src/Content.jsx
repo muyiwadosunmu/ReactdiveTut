51546c586359cf3638203bb6453c33b1d2a8008c
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2022-12-13 01:26:30 +0100

    useState in React

---
+++ src/Content.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const Content = () => {
+  const [name, setName] = useState('Muyiwa');
+  const [count, setCount] = useState(0);
+
+  const handleNameChange = () => {
+    const names = ["Bob", "Muyiwa", "Dave"];
+    const int = Math.floor(Math.random() * 3);
+    setName(names[int]);
+  };
+
+  const handleClick = () => {
+    setCount(count + 1)
+    setCount(count + 1)
+    console.log(count)
+  };
+  const handleClick2 = (name) => {
+    console.log(count);
+  };
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
+  return (
+    <main>
+      <p onDoubleClick={handleClick}>Hello {name}
+      </p>
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={handleClick2}>Click it</button>
+    </main>
+  );  
+};
+
+export default Content;
