33eb14541d1e74cfb00527ce4def6d1f775dd487
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2022-12-23 15:57:15 +0100

    Learnt how useEffect works by loading data using the local storage

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import {React, useRef } from 'react';
 import { FaPlus } from "react-icons/fa";
 
 const AddItem = ({ newItem, setNewItem, handleSubmit}) => {
+  const inputRef = useRef();
+
   return (
     <form className='addForm' onSubmit={handleSubmit}>
         <label htmlFor="addItem">Add Item</label>
         <input 
             autoFocus
+            ref={inputRef}
             id='addItem'
             type="text"
             placeholder='Add Item'
@@ -17,6 +20,7 @@ const AddItem = ({ newItem, setNewItem, handleSubmit}) => {
         <button
                 type='submit'
                 arial-label = 'Add Item'
+                onClick={() => inputRef.current.focus()}
         >
                 <FaPlus/>
         </button>
--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
+import { useState, useEffect } from 'react';
 import Header from './Header';
 import SearchItem from './SearchItem';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 
-import { useState } from 'react';
+
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
-  }
+  // Using a dependency, which only allows rendering when this dependency changes
+  useEffect(() => {
+    localStorage.setItem('shoppinglist', JSON.stringify(items));
+  }, [items])
+
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked:false, item};
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   const handleCheck =(id) => {
     const listItems = items.map((item) => item.id === id? {...item, checked:!item.checked}:item)
-    setAndSaveItems(listItems); 
+    setItems(listItems); 
   }
     
   const handleDelete =(id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   const handleSubmit = (e) => {
@@ -44,7 +46,6 @@ function App() {
     <div className="App">
       <Header title="Grocery List"/>
       <AddItem
-        setAndSaveItems = {setAndSaveItems}
         newItem = {newItem}
         setNewItem = {setNewItem}
         handleSubmit = {handleSubmit}
